713d78f924f635152b4cd1619c963099249e04b8
Monomee <hoangthth12@gmail.com>
2025-01-27 01:11:27 +0700

    run.bat is crazy

--- BookManagement/src/bookmanagement/run.bat
+++ BookManagement/src/bookmanagement/run.bat
@@ -1,12 +1,18 @@
 @echo off
 cls
 echo Compiling Java files...
-javac -d . D:\JavaNetbean\SE1942-HoangNVHE191065-AS1\BookManagement\src\bookmanagement\*.java
+
+set "BASE_DIR=%~dp0"
+
+
+javac -d "%BASE_DIR%BookManagement" "%BASE_DIR%BookManagement\src\bookmanagement\*.java"
+
 if %errorlevel% neq 0 (
     echo Compilation failed. Please check your code for errors.
     pause
     exit /b
 )
+
 echo Running program...
 java bookmanagement.Main
 pause
